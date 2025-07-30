ef7e4e4f4d12dfe18520a06263e94d62057aee0e
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2025-07-30 20:54:03 +0800

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 
+import globals from 'globals';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
@@ -8,6 +9,11 @@ export default tseslint.config(
   eslint.configs.recommended,
   eslintPluginPrettierRecommended,
   tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: { ...globals.node },
+    },
+  },
   {
     ignores: ['node_modules', 'dist'],
   },
