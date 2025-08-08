6f77002fdd23ed7f275dd0c8871051303437d54a
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2025-08-08 16:28:30 +0800

    update

--- src/initializeCryptoEngine.ts
+++ src/initializeCryptoEngine.ts
@@ -1,5 +1,6 @@
 import crypto from 'crypto';
 import { CryptoEngine, setEngine } from 'pkijs';
+import { cryptoProvider, CryptoProvider } from '@peculiar/x509';
 
 export function initializeCryptoEngine() {
   if (typeof crypto !== 'undefined') {
@@ -9,10 +10,12 @@ export function initializeCryptoEngine() {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const nodeCrypto = (crypto as any).webcrypto as Crypto;
       setEngine(name, new CryptoEngine({ name, crypto: nodeCrypto }));
+      cryptoProvider.set(CryptoProvider.DEFAULT, nodeCrypto);
     } else if ('subtle' in crypto) {
       // NodeJS ^19
       const name = 'NodeJS ^19';
       setEngine(name, new CryptoEngine({ name, crypto }));
+      cryptoProvider.set(CryptoProvider.DEFAULT, crypto);
     }
   }
 }
