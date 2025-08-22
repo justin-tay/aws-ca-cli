0ce44ee86c955560f2a6538cb79298f04830e034
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2025-08-22 16:59:51 +0800

    update

--- src/exportPkcs7CertificateChain.ts
+++ src/exportPkcs7CertificateChain.ts
@@ -1,8 +1,10 @@
 import { PemConverter, X509Certificate } from '@peculiar/x509';
 import { exportPkcs7CertificateChainBinary } from './exportPkcs7CertificateChainBinary';
+import { SignedData } from 'pkijs';
 
 export async function exportPkcs7CertificateChain(params: {
   certificateChain: X509Certificate[];
+  signer?: (signedData: SignedData) => void;
 }) {
   const contentInfoBinary = await exportPkcs7CertificateChainBinary(params);
   return PemConverter.encode(contentInfoBinary, 'PKCS7');
--- src/exportPkcs7CertificateChainBinary.ts
+++ src/exportPkcs7CertificateChainBinary.ts
@@ -12,8 +12,9 @@ import { fromBER } from 'asn1js';
 
 export async function exportPkcs7CertificateChainBinary(params: {
   certificateChain: X509Certificate[];
+  signer?: (signedData: SignedData) => void;
 }) {
-  const { certificateChain } = params;
+  const { certificateChain, signer } = params;
   const certificates: CertificateSetItem[] = [];
   certificateChain.forEach((certificate) => {
     certificates.push(
@@ -22,15 +23,19 @@ export async function exportPkcs7CertificateChainBinary(params: {
       }),
     );
   });
+  const content = new SignedData({
+    version: 1,
+    encapContentInfo: new EncapsulatedContentInfo({
+      eContentType: id_ContentType_Data, // "data" content type
+    }),
+    certificates,
+  });
+  if (signer) {
+    signer(content);
+  }
   const contentInfo = new ContentInfo({
     contentType: id_ContentType_SignedData,
-    content: new SignedData({
-      version: 1,
-      encapContentInfo: new EncapsulatedContentInfo({
-        eContentType: id_ContentType_Data, // "data" content type
-      }),
-      certificates,
-    }).toSchema(true),
+    content: content.toSchema(true),
   });
 
   return contentInfo.toSchema().toBER(false);
